e7317f1bdbb82a2c5f0ea275b07f02c0a299a4a4
ivanshum <ivanshum@mustbefamily.com>
2023-10-15 02:11:13 +0600

    timer, and change animation

--- src/components/section1.js
+++ src/components/section1.js
@@ -1,15 +1,16 @@
 import * as React from "react"
 import Lottie from "lottie-react";
-import ScrollDownLottie from "../lottie/animation_lnqazmry.json";
+import ScrollDownLottie from "../lottie/white_stripes_down.json";
+import Timer from "./timer";
 export default function Section1 () {
   return (
     <section id="section-1" className="wfull h-screen flex flex-col justify-end items-center pb-3">
     <div className="flex items-center flex-col text-white gap-1">
+      <Timer deadline={new Date(Date.now()+43*24*60*60*1000).toString()}/>
       <Lottie animationData={ScrollDownLottie} 
         loop autoPlay
-        className="w-10"
+        className="w-20"
       />
-      Scroll down
     </div>
   </section>
 )
