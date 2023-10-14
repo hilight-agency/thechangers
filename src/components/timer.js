6b0b6009cb482ca2c2ad0f43814e8f63757e5b57
ivanshum-mbf <ivanshum@mustbefamily.com>
2023-10-15 02:18:17 +0600

    5 add startvideo with scroll indicator (#7)
    * first things first
    
    * first screen
    
    * final 1 step
    
    * timer, and change animation

---
+++ src/components/timer.js
@@ -0,0 +1,39 @@
+import * as React from "react"
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+export default function Timer ({ deadline = new Date().toString() }) {
+    const parsedDeadline = React.useMemo(() => Date.parse(deadline), [deadline]);
+    const [time, setTime] = React.useState(parsedDeadline - Date.now());
+
+    React.useEffect(() => {
+        const interval = setInterval(
+            () => setTime(parsedDeadline - Date.now()),
+            1000,
+        );
+
+        return () => clearInterval(interval);
+    }, [parsedDeadline]);
+
+    return (
+        <div className="flex justify-between text-2xl gap-4">
+            {Object.entries({
+                D: time / DAY,
+                H: (time / HOUR) % 24,
+                M: (time / MINUTE) % 60,
+                S: (time / SECOND) % 60,
+            }).map(([label, value],i,a) => (
+<>                <div key={label} className="">
+                    <div className="flex items-center flex-col">
+                        <p>{`${Math.floor(value)}`.padStart(2, "0")}</p>
+                        <span className="">{label}</span>
+                    </div>
+                </div>
+                {i!==a.length-1 && <div className="leading-tight">:</div>}</>
+            ))}
+        </div>
+    );
+};
