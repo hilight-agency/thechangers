41a1599008fa10b81dbbfabe579395dc34a37bbf
ivanshum <ivanshum@mustbefamily.com>
2023-10-14 23:41:07 +0600

    first things first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,34 @@
 import * as React from "react"
+import Lottie from "lottie-react";
+import Preloader from "../videos/preload.mp4"
+import ScrollDownLottie from "../lottie/animation_lnqazmry.json"
+import PreloaderPoster from "../images/tc_main_m.jpg"
 const IndexPage = () => {
+  const videoref=React.useRef()
   return (
-    <main className="w-full h-full m-0 p-0 text-slate-300 bg-black">
-      <h1>Hi there!</h1>
-    </main>
+<>
+      <video 
+      ref={videoref}
+        id="main-video" 
+        data-filename=""
+        className="object-cover object-center align-middle left-0 top-0 w-full h-screen fixed z-10" 
+        poster={PreloaderPoster}
+        playsInline 
+        autoPlay 
+        muted
+        loop 
+        src={Preloader}>
+      </video>
+      <div id="wrapper" className="w-full relative z-20">
+        <section id="section-1" className="wfull h-screen flex flex-col justify-end items-center pb-3">
+          <Lottie animationData={ScrollDownLottie} 
+            loop autoPlay
+            className="w-10"
+          />
+        </section>
+      </div>
+      {/* </div> */}
+    </>
   )
 }
 
