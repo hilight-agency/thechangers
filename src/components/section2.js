ce8ffdf1a3c93c93da0dec2dbe1a95098b2cbea6
ivanshum <ivanshum@mustbefamily.com>
2023-10-15 20:40:52 +0600

    mainfiles

--- src/components/section2.js
+++ src/components/section2.js
@@ -1,18 +1,26 @@
 import { Controller, Scene } from "react-scrollmagic";
 import * as React from "react"
+import Sequence from "./sequence/ImageSequence";
+
 export default function Section2 () {
     const sectionRef = React.useRef()
-  return (
+    const ref = React.useRef();
+return (
 <>
-<div className="wfull h-screen"></div>
-    <section id="section-2" className="w-full h-[200vh] flex flex-col justify-end items-center pb-3" ref={sectionRef}>
+    <div className="w-full h-screen"></div>
+    <section id="section-2" className="w-full h-screen" ref={sectionRef}>
       <Controller>
-        <Scene duration="200%" triggerHook="onEnter" triggerElement={sectionRef} pin>
-          {progress => (
-            <div className="w-full h-screen">
-              <img src="mainsq/mainsq000.webp" style={{opacity:progress*5}} width={'100%'} height={'100%'} className="object-cover"/>
+        <Scene duration="400%" triggerHook="onLeave" pin>
+          {progress => { 
+            console.log(progress)
+            return (
+/*             <div className="w-full h-screen relative">
+              <img src="mainsq/mainsq000.webp" style={{opacity:progress*5}} className="object-cover absolute w-full h-screen top-0 bottom-0"/>
             </div>
-          )}
+            
+ */         <div style={{ height: "100vh", position: "relative", opacity: progress*20 }}>
+              <Sequence ref={ref} progress={progress} />
+            </div>)}}
         </Scene>
       </Controller>
     </section>
---
+++ src/components/sequence/mainsq/index.js
@@ -0,0 +1,391 @@
+import A0 from "./mainsq000.webp";
+import A1 from "./mainsq001.webp";
+import A2 from "./mainsq002.webp";
+import A3 from "./mainsq003.webp";
+import A4 from "./mainsq004.webp";
+import A5 from "./mainsq005.webp";
+import A6 from "./mainsq006.webp";
+import A7 from "./mainsq007.webp";
+import A8 from "./mainsq008.webp";
+import A9 from "./mainsq009.webp";
+
+import A10 from "./mainsq010.webp";
+import A11 from "./mainsq011.webp";
+import A12 from "./mainsq012.webp";
+import A13 from "./mainsq013.webp";
+import A14 from "./mainsq014.webp";
+import A15 from "./mainsq015.webp";
+import A16 from "./mainsq016.webp";
+import A17 from "./mainsq017.webp";
+import A18 from "./mainsq018.webp";
+import A19 from "./mainsq019.webp";
+
+import A20 from "./mainsq020.webp";
+import A21 from "./mainsq021.webp";
+import A22 from "./mainsq022.webp";
+import A23 from "./mainsq023.webp";
+import A24 from "./mainsq024.webp";
+import A25 from "./mainsq025.webp";
+import A26 from "./mainsq026.webp";
+import A27 from "./mainsq027.webp";
+import A28 from "./mainsq028.webp";
+import A29 from "./mainsq029.webp";
+
+import A30 from "./mainsq030.webp";
+import A31 from "./mainsq031.webp";
+import A32 from "./mainsq032.webp";
+import A33 from "./mainsq033.webp";
+import A34 from "./mainsq034.webp";
+import A35 from "./mainsq035.webp";
+import A36 from "./mainsq036.webp";
+import A37 from "./mainsq037.webp";
+import A38 from "./mainsq038.webp";
+import A39 from "./mainsq039.webp";
+
+import A40 from "./mainsq040.webp";
+import A41 from "./mainsq041.webp";
+import A42 from "./mainsq042.webp";
+import A43 from "./mainsq043.webp";
+import A44 from "./mainsq044.webp";
+import A45 from "./mainsq045.webp";
+import A46 from "./mainsq046.webp";
+import A47 from "./mainsq047.webp";
+import A48 from "./mainsq048.webp";
+import A49 from "./mainsq049.webp";
+
+import A50 from "./mainsq050.webp";
+import A51 from "./mainsq051.webp";
+import A52 from "./mainsq052.webp";
+import A53 from "./mainsq053.webp";
+import A54 from "./mainsq054.webp";
+import A55 from "./mainsq055.webp";
+import A56 from "./mainsq056.webp";
+import A57 from "./mainsq057.webp";
+import A58 from "./mainsq058.webp";
+import A59 from "./mainsq059.webp";
+
+import A60 from "./mainsq060.webp";
+import A61 from "./mainsq061.webp";
+import A62 from "./mainsq062.webp";
+import A63 from "./mainsq063.webp";
+import A64 from "./mainsq064.webp";
+import A65 from "./mainsq065.webp";
+import A66 from "./mainsq066.webp";
+import A67 from "./mainsq067.webp";
+import A68 from "./mainsq068.webp";
+import A69 from "./mainsq069.webp";
+
+import A70 from "./mainsq070.webp";
+import A71 from "./mainsq071.webp";
+import A72 from "./mainsq072.webp";
+import A73 from "./mainsq073.webp";
+import A74 from "./mainsq074.webp";
+import A75 from "./mainsq075.webp";
+import A76 from "./mainsq076.webp";
+import A77 from "./mainsq077.webp";
+import A78 from "./mainsq078.webp";
+import A79 from "./mainsq079.webp";
+
+import A80 from "./mainsq080.webp";
+import A81 from "./mainsq081.webp";
+import A82 from "./mainsq082.webp";
+import A83 from "./mainsq083.webp";
+import A84 from "./mainsq084.webp";
+import A85 from "./mainsq085.webp";
+import A86 from "./mainsq086.webp";
+import A87 from "./mainsq087.webp";
+import A88 from "./mainsq088.webp";
+import A89 from "./mainsq089.webp";
+
+import A90 from "./mainsq090.webp";
+import A91 from "./mainsq091.webp";
+import A92 from "./mainsq092.webp";
+import A93 from "./mainsq093.webp";
+import A94 from "./mainsq094.webp";
+import A95 from "./mainsq095.webp";
+import A96 from "./mainsq096.webp";
+import A97 from "./mainsq097.webp";
+import A98 from "./mainsq098.webp";
+import A99 from "./mainsq099.webp";
+
+import A100 from "./mainsq100.webp";
+import A101 from "./mainsq101.webp";
+import A102 from "./mainsq102.webp";
+import A103 from "./mainsq103.webp";
+import A104 from "./mainsq104.webp";
+import A105 from "./mainsq105.webp";
+import A106 from "./mainsq106.webp";
+import A107 from "./mainsq107.webp";
+import A108 from "./mainsq108.webp";
+import A109 from "./mainsq109.webp";
+
+import A110 from "./mainsq110.webp";
+import A111 from "./mainsq111.webp";
+import A112 from "./mainsq112.webp";
+import A113 from "./mainsq113.webp";
+import A114 from "./mainsq114.webp";
+import A115 from "./mainsq115.webp";
+import A116 from "./mainsq116.webp";
+import A117 from "./mainsq117.webp";
+import A118 from "./mainsq118.webp";
+import A119 from "./mainsq119.webp";
+
+import A120 from "./mainsq120.webp";
+import A121 from "./mainsq121.webp";
+import A122 from "./mainsq122.webp";
+import A123 from "./mainsq123.webp";
+import A124 from "./mainsq124.webp";
+import A125 from "./mainsq125.webp";
+import A126 from "./mainsq126.webp";
+import A127 from "./mainsq127.webp";
+import A128 from "./mainsq128.webp";
+import A129 from "./mainsq129.webp";
+
+import A130 from "./mainsq130.webp";
+import A131 from "./mainsq131.webp";
+import A132 from "./mainsq132.webp";
+import A133 from "./mainsq133.webp";
+import A134 from "./mainsq134.webp";
+import A135 from "./mainsq135.webp";
+import A136 from "./mainsq136.webp";
+import A137 from "./mainsq137.webp";
+import A138 from "./mainsq138.webp";
+import A139 from "./mainsq139.webp";
+
+import A140 from "./mainsq140.webp";
+import A141 from "./mainsq141.webp";
+import A142 from "./mainsq142.webp";
+import A143 from "./mainsq143.webp";
+import A144 from "./mainsq144.webp";
+import A145 from "./mainsq145.webp";
+import A146 from "./mainsq146.webp";
+import A147 from "./mainsq147.webp";
+import A148 from "./mainsq148.webp";
+import A149 from "./mainsq149.webp";
+
+import A150 from "./mainsq150.webp";
+import A151 from "./mainsq151.webp";
+import A152 from "./mainsq152.webp";
+import A153 from "./mainsq153.webp";
+import A154 from "./mainsq154.webp";
+import A155 from "./mainsq155.webp";
+import A156 from "./mainsq156.webp";
+import A157 from "./mainsq157.webp";
+import A158 from "./mainsq158.webp";
+import A159 from "./mainsq159.webp";
+
+import A160 from "./mainsq160.webp";
+import A161 from "./mainsq161.webp";
+import A162 from "./mainsq162.webp";
+import A163 from "./mainsq163.webp";
+import A164 from "./mainsq164.webp";
+import A165 from "./mainsq165.webp";
+import A166 from "./mainsq166.webp";
+import A167 from "./mainsq167.webp";
+import A168 from "./mainsq168.webp";
+import A169 from "./mainsq169.webp";
+
+import A170 from "./mainsq170.webp";
+import A171 from "./mainsq171.webp";
+import A172 from "./mainsq172.webp";
+import A173 from "./mainsq173.webp";
+import A174 from "./mainsq174.webp";
+import A175 from "./mainsq175.webp";
+import A176 from "./mainsq176.webp";
+
+export const MainSq = Array.of(
+  A0,
+  A1,
+  A2,
+  A3,
+  A4,
+  A5,
+  A6,
+  A7,
+  A8,
+  A9,
+
+  A10,
+  A11,
+  A12,
+  A13,
+  A14,
+  A15,
+  A16,
+  A17,
+  A18,
+  A19,
+
+  A20,
+  A21,
+  A22,
+  A23,
+  A24,
+  A25,
+  A26,
+  A27,
+  A28,
+  A29,
+
+  A30,
+  A31,
+  A32,
+  A33,
+  A34,
+  A35,
+  A36,
+  A37,
+  A38,
+  A39,
+
+  A40,
+  A41,
+  A42,
+  A43,
+  A44,
+  A45,
+  A46,
+  A47,
+  A48,
+  A49,
+
+  A50,
+  A51,
+  A52,
+  A53,
+  A54,
+  A55,
+  A56,
+  A57,
+  A58,
+  A59,
+
+  A60,
+  A61,
+  A62,
+  A63,
+  A64,
+  A65,
+  A66,
+  A67,
+  A68,
+  A69,
+
+  A70,
+  A71,
+  A72,
+  A73,
+  A74,
+  A75,
+  A76,
+  A77,
+  A78,
+  A79,
+
+  A80,
+  A81,
+  A82,
+  A83,
+  A84,
+  A85,
+  A86,
+  A87,
+  A88,
+  A89,
+
+  A90,
+  A91,
+  A92,
+  A93,
+  A94,
+  A95,
+  A96,
+  A97,
+  A98,
+  A99,
+
+  A100,
+  A101,
+  A102,
+  A103,
+  A104,
+  A105,
+  A106,
+  A107,
+  A108,
+  A109,
+
+  A110,
+  A111,
+  A112,
+  A113,
+  A114,
+  A115,
+  A116,
+  A117,
+  A118,
+  A119,
+
+  A120,
+  A121,
+  A122,
+  A123,
+  A124,
+  A125,
+  A126,
+  A127,
+  A128,
+  A129,
+
+  A130,
+  A131,
+  A132,
+  A133,
+  A134,
+  A135,
+  A136,
+  A137,
+  A138,
+  A139,
+
+  A140,
+  A141,
+  A142,
+  A143,
+  A144,
+  A145,
+  A146,
+  A147,
+  A148,
+  A149,
+
+  A150,
+  A151,
+  A152,
+  A153,
+  A154,
+  A155,
+  A156,
+  A157,
+  A158,
+  A159,
+
+  A160,
+  A161,
+  A162,
+  A163,
+  A164,
+  A165,
+  A166,
+  A167,
+  A168,
+  A169,
+
+  A170,
+  A171,
+  A172,
+  A173,
+  A174,
+  A175,
+  A176
+);
