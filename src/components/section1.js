bc92918fd2a67edfe1367028e0995c7dca913945
ivanshum-mbf <ivanshum@mustbefamily.com>
2023-10-19 00:53:46 +0600

    temp (#11)

--- src/components/section1.js
+++ src/components/section1.js
@@ -5,8 +5,8 @@ import Timer from './timer';
 import Sequence from './sequence/ImageSequence';
 import { Controller, Scene } from 'react-scrollmagic';
 
-export default function Section1() {
-  const [progress, setProgress] = React.useState(0.0);
+export default function Section1({ videoref }) {
+  const [progressstate, setProgress] = React.useState(0.0);
   const sectionRef = React.useRef();
   const ref = React.useRef();
   return (
@@ -15,19 +15,31 @@ export default function Section1() {
         id="section-1"
         className="w-full h-screen flex flex-col justify-end items-center pb-3 fixed z-30"
       >
-        <div className="flex items-center flex-col gap-1" style={{ opacity: 1.2 - progress * 5 }}>
+        <div
+          className="flex items-center flex-col gap-1"
+          style={{ opacity: 1.2 - progressstate * 5 }}
+        >
           <Timer deadline={new Date(Date.now() + 43 * 24 * 60 * 60 * 1000).toString()} />
           <Lottie animationData={ScrollDownLottie} loop autoPlay className="w-20" />
         </div>
       </section>
-      <div className="w-full h-screen"></div>
-      <section id="section-2" className="w-full h-screen relative z-20" ref={sectionRef}>
+      <section id="section-2" className="w-full h-[200vh] relative z-20" ref={sectionRef}>
         <Controller>
           <Scene duration="200%" triggerHook="onLeave" pin>
             {(progress) => {
               setProgress(progress);
+              if (videoref.current)
+                videoref.current.style.display = progress > 0.5 ? 'none' : 'block';
+
+              console.log(videoref.current?.style.opacity);
+
               return (
-                <div style={{ height: '100vh', position: 'relative', opacity: progress * 20 }}>
+                <div
+                  className={'h-screen relative'}
+                  style={{
+                    opacity: progress < 0.87 ? progress * 20 : 5 + 1 - (progress + 0.22) / 0.2,
+                  }}
+                >
                   <Sequence ref={ref} progress={progress} />
                 </div>
               );
