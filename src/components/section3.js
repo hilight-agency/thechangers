5d0aa1c3adb224cb53c0a56b3c7fece78dd986ef
ivanshum <ivanshum@mustbefamily.com>
2023-10-21 13:37:36 +0600

    filler trigger

--- src/components/section3.js
+++ src/components/section3.js
@@ -3,12 +3,21 @@ import { ScrollTrigger, Timeline, Tween } from 'react-gsap';
 
 export default function Section3() {
   const texts = [
-    '"The Changers" is a unique event that gathers investors, decision makers, and celebrities from all over the world at the Meydan Hotel. It provides opportunities for entrepreneurs, companies and influencers, including access to politicians and entertainment stars.',
-    'Venture capitalists and investors are also present, ready to support innovation. The participation of the royal families adds status to the event, making it a platform for contacts and partnerships.',
-    '"The Changers" is a catalyst for change and an opportunity to establish the future of trade and influence.',
+    {
+      text: '"The Changers" is a unique event that gathers investors, decision makers, and celebrities from all over the world at the Meydan Hotel. It provides opportunities for entrepreneurs, companies and influencers, including access to politicians and entertainment stars.',
+      class: 'text-left',
+    },
+    {
+      text: 'Venture capitalists and investors are also present, ready to support innovation. The participation of the royal families adds status to the event, making it a platform for contacts and partnerships.',
+      class: 'text-right',
+    },
+    {
+      text: '"The Changers" is a catalyst for change and an opportunity to establish the future of trade and influence.',
+      class: 'text-center',
+    },
   ];
   return (
-    <>
+    <React.Fragment>
       <div id='section3' className='w-full h-screen flex flex-col items-center'>
         <ScrollTrigger
           start='center bottom'
@@ -17,13 +26,16 @@ export default function Section3() {
           id='section3scroll'
           pin={'#section3'}
           pinSpacing={false}
+          endTrigger={'#section3trigger'}
           trigger={'#section3'}
           scrub
         >
           <Timeline
             playState='pause'
             target={texts.map((item, inx) => (
-              <p key={inx}>{item}</p>
+              <p key={inx} className={`${item.class} uppercase text-xl`}>
+                {item.text}
+              </p>
             ))}
           >
             {texts.map((...params) => (
@@ -34,7 +46,7 @@ export default function Section3() {
                   duration={10}
                   target={params[1]}
                 />
-                <Tween to={{ y: '-15%' }} duration={30} target={params[1]} />
+                <Tween to={{ y: '-50%' }} duration={30} target={params[1]} />
                 <Tween to={{ opacity: 0, y: '-100%' }} duration={10} target={params[1]} />
                 <Tween to={{ display: 'none' }} duration={0} target={params[1]} />
               </>
@@ -42,6 +54,7 @@ export default function Section3() {
           </Timeline>
         </ScrollTrigger>
       </div>
-    </>
+      <div id='section3trigger' className='w-full h-[300vh]'></div>
+    </React.Fragment>
   );
 }
