115905d70cc4dd684ffb929346fcefe504040474
ivanshum <ivanshum@mustbefamily.com>
2023-10-23 17:42:18 +0600

    shooting stars

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import Section1 from '../components/section1';
 import Section2 from '../components/section2';
 import Section3 from '../components/section3';
+import Stars from '../components/stars';
 
 const IndexPage = () => {
   return (
     <div id='wrapper' className='w-full relative z-20 text-white'>
+      <Stars />
       <Section1 />
       <Section2 />
       <Section3 />
