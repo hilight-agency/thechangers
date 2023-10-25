24e8060a3a7b12982b21a05971f7d714777300e3
ivanshum <ivanshum@mustbefamily.com>
2023-10-25 09:56:41 +0600

    stars refactor and attach to scroll

--- src/components/section5.js
+++ src/components/section5.js
@@ -6,7 +6,7 @@ export default function Section5() {
   const setProgress = useStarsStore((state) => state.setProgress);
 
   return (
-    <div id='section5' className='w-full h-[300vh] flex flex-col items-center relative z-40'>
+    <div id='section5' className='w-full h-[700vh] flex flex-col items-center relative z-40'>
       <ScrollTrigger
         start='top top'
         end='bottom top'
