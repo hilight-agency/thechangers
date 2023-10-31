3e24a9d026d5c6aeb82b11c60d993d47541364d4
ivanshum <ivanshum@mustbefamily.com>
2023-10-31 19:56:07 +0600

    test click

--- src/components/section8.js
+++ src/components/section8.js
@@ -2,11 +2,18 @@ import * as React from 'react';
 import data from '../speakers.json';
 import SpeakerItem from './speakerItem';
 export default function Section8() {
+  const [opened, setOpened] = React.useState(false);
+
   return (
     <React.Fragment>
       <div className='w-full p-5 flex flex-col gap-5'>
         {data.items.map((element) => (
-          <SpeakerItem key={element.id + 'speaker'} element={element} />
+          <SpeakerItem
+            key={element.id + 'speaker'}
+            isOpened={opened}
+            element={element}
+            onClick={() => setOpened((current) => (element.id != current ? element.id : false))}
+          />
         ))}
       </div>
     </React.Fragment>
--- src/components/speakerItem.js
+++ src/components/speakerItem.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
-const RoundRight = (num) => Math.round((num + Number.EPSILON) * 100) / 100;
-
-export default function SpeakerItem({ element }) {
-  const [prog, setProg] = React.useState(0.1);
-  const thresholdArr = Array.from({ length: 100 }, (_, i) => RoundRight(i / 100));
+export default function SpeakerItem({ element, onClick, isOpened }) {
+  const [prog, setProg] = React.useState(0);
+  const thresholdArr = Array.from({ length: 100 }, (_, i) => i / 100);
   const { ...inview } = useInView({
     threshold: thresholdArr,
     onChange: (...attr) => {
-      setProg(RoundRight(attr[1]?.intersectionRatio * 2 - 1 || 0));
+      setProg(() => attr[1]?.intersectionRatio * 2 - 1 || 0);
     },
   });
+  React.useEffect(() => {
+    if (inview && inview[2]) {
+      inview[2].target.style.opacity = prog;
+      inview[2].target.style.transform = `scale(${0.75 + prog * 0.25})`;
+    }
+  }, [prog]);
   return (
     <div
       ref={inview.ref}
-      className='w-full h-28 max-h-28 flex flex-row gap-5 px-3 py-1'
+      className={`w-full ${
+        isOpened === element.id ? 'h-screen max-h-screen' : 'h-28 max-h-28'
+      } flex flex-row gap-5 px-3 py-1 relative z-40`}
       style={{
         background: 'rgba(217, 217, 217, 0.20)',
-        opacity: prog,
-        transform: `scale(${0.25 + prog * 0.75})`,
       }}
+      onClick={onClick}
     >
       <div className='flex flex-col items-center justify-center min-w-[78px] min-h-[78px]'>
         <img src={`/speakers/${element.img}.png`} width={78} height={78} />
