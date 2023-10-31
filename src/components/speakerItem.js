b8fd7f86cef355a696da4b9856131484734aa21d
ivanshum <ivanshum@mustbefamily.com>
2023-10-31 18:44:26 +0600

    update animation

--- src/components/speakerItem.js
+++ src/components/speakerItem.js
@@ -4,9 +4,13 @@ import { useInView } from 'react-intersection-observer';
 const RoundRight = (num) => Math.round((num + Number.EPSILON) * 100) / 100;
 
 export default function SpeakerItem({ element }) {
+  const [prog, setProg] = React.useState(0.1);
   const thresholdArr = Array.from({ length: 100 }, (_, i) => RoundRight(i / 100));
   const { ...inview } = useInView({
     threshold: thresholdArr,
+    onChange: (...attr) => {
+      setProg(RoundRight(attr[1]?.intersectionRatio * 2 - 1 || 0));
+    },
   });
   return (
     <div
@@ -14,7 +18,8 @@ export default function SpeakerItem({ element }) {
       className='w-full h-28 max-h-28 flex flex-row gap-5 px-3 py-1'
       style={{
         background: 'rgba(217, 217, 217, 0.20)',
-        opacity: RoundRight(inview?.entry?.intersectionRatio || 0),
+        opacity: prog,
+        transform: `scale(${0.25 + prog * 0.75})`,
       }}
     >
       <div className='flex flex-col items-center justify-center min-w-[78px] min-h-[78px]'>
