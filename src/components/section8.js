05b4b577c9ce083f79a3203d8fb0cd798dc32429
ivanshum <ivanshum@mustbefamily.com>
2023-10-31 22:06:59 +0600

    lock scroll

--- src/components/section8.js
+++ src/components/section8.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import data from '../speakers.json';
 import SpeakerItem from './speakerItem';
+import useScrollLock from '../hooks/useScrollLock';
 export default function Section8() {
   const [opened, setOpened] = React.useState(false);
-
+  const [lockScroll, unlockScroll] = useScrollLock();
   return (
     <React.Fragment>
       <div className='w-full p-5 flex flex-col gap-5'>
@@ -12,7 +13,17 @@ export default function Section8() {
             key={element.id + 'speaker'}
             isOpened={opened}
             element={element}
-            onClick={() => setOpened((current) => (element.id != current ? element.id : false))}
+            onClick={() =>
+              setOpened((current) => {
+                if (element.id != current) {
+                  lockScroll();
+                  return element.id;
+                } else {
+                  unlockScroll();
+                  return false;
+                }
+              })
+            }
           />
         ))}
       </div>
